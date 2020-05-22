a2731241e4f11a85956374f68a667b8dbd5f9f57
Max Resnick <max.resnick@forgerock.com>
2020-05-22 14:05:37 -0700

    feat: load existing secrets from Azure key vault

--- api/v1alpha1/secretagentconfiguration_webhook.go
+++ api/v1alpha1/secretagentconfiguration_webhook.go
@@ -5,15 +5,12 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package v1alpha1
 
 import (
@@ -95,12 +92,15 @@ func ConfigurationStructLevelValidator(sl validator.StructLevel) {
 		if config.AppConfig.GCPProjectID == "" {
 			sl.ReportError(config.AppConfig.GCPProjectID, "gcpProjectID", "GCPProjectID", "emptyGCPProjectID", "")
 		}
+	case SecretsManagerAzure:
+		if config.AppConfig.AzureVaultName == "" {
+			sl.ReportError(config.AppConfig.AzureVaultName, "azureVaultName", "AzureVaultName", "emptyAzureVaultName", "")
+		}
 	case SecretsManagerAWS:
 		if config.AppConfig.AWSRegion == "" {
 			sl.ReportError(config.AppConfig.AWSRegion, "awsRegion", "AWSRegion", "emptyAWSRegion", "")
 		}
 	}
-
 	// Secrets
 	for secretIndex, secret := range config.Secrets {
 		for keyIndex, key := range secret.Keys {
