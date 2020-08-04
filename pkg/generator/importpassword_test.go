a20af670fff030eb3a5941c11834333cc3fd651f
Max Resnick <max.resnick@forgerock.com>
2020-08-04 11:36:14 -0700

    tests for importpass alias manager
    closes #74

---
+++ pkg/generator/importpassword_test.go
@@ -0,0 +1,81 @@
+package generator
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/ForgeRock/secret-agent/api/v1alpha1"
+)
+
+func TestImportPassword(t *testing.T) {
+	length := 32
+	kc := &v1alpha1.KeyConfig{
+		Name: "testimportpass",
+		Type: "password",
+		Spec: &v1alpha1.KeySpec{
+			Length: &length,
+		},
+	}
+	pwdSpec := &v1alpha1.KeyConfig{
+		Name: "testConfig",
+		Type: "keytool",
+		Spec: &v1alpha1.KeySpec{
+			StorePassPath: "storepass/pass",
+			StoreType:     "pkcs12",
+			KeyPassPath:   "keypass/pass",
+			KeytoolAliases: []*v1alpha1.KeytoolAliasConfig{
+				&v1alpha1.KeytoolAliasConfig{
+					Name:       "testimportpass",
+					Cmd:        "importpassword",
+					SourcePath: "testpass/pass",
+				},
+			},
+		},
+	}
+	keyToolMgr, err := NewKeyTool(pwdSpec)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pwdMgr, err := NewPassword(kc)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = pwdMgr.Generate()
+	if err != nil {
+		t.Fatal(err)
+	}
+	keyToolMgr.References()
+	keyToolMgr.LoadReferenceData(map[string][]byte{
+		"pass": pwdMgr.Value,
+	})
+	err = keyToolMgr.Generate()
+	if err != nil {
+		t.Fatal(err)
+	}
+	baseArgs := []string{
+		"-storetype", string(keyToolMgr.V1Spec.StoreType),
+		"-storepass", keyToolMgr.storePassValue,
+		"-keypass", keyToolMgr.keyPassValue,
+		"-keystore", keyToolMgr.storePath,
+	}
+	baseCmd := keyToolMgr.baseCommand(*keytoolPath, baseArgs)
+	args := []string{
+		"-alias", "testimportpass",
+	}
+	if _, err := os.Stat(keyToolMgr.storePath); !os.IsNotExist(err) {
+		t.Error("expected keyToolMgr to cleanup store but didn't")
+	}
+	ioutil.WriteFile(keyToolMgr.storePath, keyToolMgr.storeBytes, 0600)
+	defer os.RemoveAll(keyToolMgr.storePath)
+	cmd := baseCmd("-list", args)
+	results, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(string(results))
+	}
+	if !strings.Contains(string(results), string(pwdMgr.Name)) {
+		t.Errorf("Expected Alias %s to exist but found: \n %s", string(pwdMgr.Name), string(results))
+	}
+}
