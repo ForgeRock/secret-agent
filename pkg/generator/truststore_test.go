ffd15f72eed5aadc8addd9f03644ff21960e2c39
Max Resnick <max.resnick@forgerock.com>
2020-08-07 11:58:20 -0700

    adds truststore type, cleans up a couple of keytool issues
    closes #76

---
+++ pkg/generator/truststore_test.go
@@ -0,0 +1,44 @@
+package generator
+
+import (
+	"testing"
+
+	"github.com/ForgeRock/secret-agent/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"software.sslmate.com/src/go-pkcs12"
+)
+
+func TestTrustStore(t *testing.T) {
+	rootCA, err := makeTestNewRootCA(t)
+	if err != nil {
+		t.Error(err)
+	}
+	rootCA.Generate()
+	testSecret := &corev1.Secret{}
+	rootCA.ToKubernetes(testSecret)
+	key := &v1alpha1.KeyConfig{
+		Name: "myname",
+		Type: v1alpha1.KeyConfigTypeTrustStore,
+		Spec: &v1alpha1.KeySpec{
+			TruststoreImportPaths: []string{"testconfig/ca"},
+		},
+	}
+	tsMgr, err := NewTrustStore(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if empty := tsMgr.IsEmpty(); !empty {
+		t.Fatalf("expected no error found: %s", err)
+	}
+	tsMgr.References()
+	tsMgr.LoadReferenceData(testSecret.Data)
+	tsMgr.Generate()
+	parsed, err := pkcs12.DecodeTrustStore(tsMgr.Value, "changeit")
+	if err != nil {
+		t.Fatal(err)
+	}
+	lastCert := parsed[len(parsed)-1]
+	if lastCert.Subject.CommonName != "foo" {
+		t.Errorf("expected to find cert with common name of 'foobar' but found %+v", lastCert.Subject.CommonName)
+	}
+}
