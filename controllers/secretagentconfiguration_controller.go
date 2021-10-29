a30140536ec5e6f4eccdf46248b74e62a1896152
jrcast <jrcast@users.noreply.github.com>
2021-10-28 17:47:00 -0700

    Fix: Update RBAC to improve openShift support
    1. Update RBAC to improve openShift support
    1. Assign * permissions to the /finalizers endpoint

--- controllers/secretagentconfiguration_controller.go
+++ controllers/secretagentconfiguration_controller.go
@@ -49,9 +49,9 @@ type SecretAgentConfigurationReconciler struct {
 
 // +kubebuilder:rbac:groups=secret-agent.secrets.forgerock.io,resources=secretagentconfigurations,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=secret-agent.secrets.forgerock.io,resources=secretagentconfigurations/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=secret-agent.secrets.forgerock.io,resources=secretagentconfigurations/finalizers,verbs=update
+// +kubebuilder:rbac:groups=secret-agent.secrets.forgerock.io,resources=secretagentconfigurations/finalizers,verbs=*
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups="",resources=secrets/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=secrets/finalizers,verbs=*
 // +kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=validatingwebhookconfigurations;mutatingwebhookconfigurations,verbs=get;update;patch
 
 // Reconcile reconcile loop for CRD controller
